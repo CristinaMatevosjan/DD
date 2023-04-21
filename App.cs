8852bbd7ebaffc22358e2fba3ebae1cc0bc71e78
Kristina M <c.listeva@yandex.ru>
2023-04-21 21:41:22 +0300

    first

---
+++ App.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequencyDictionary
+{
+    class App
+    {
+        public void Start(string fail)
+        {
+            try
+            {
+                string text = File.ReadAllText(fail);
+                //Массив символов разделителей
+                char[] separator = { '.', ',', ';', ':', '-', ')', '(', '\n', '\r', '\t', '?', '!', '`', '\'', '"', ' ', '<','>','/',']','[','_','&','#','№','—','.','\u00AB','\u00BB','\u2026','\u2012','\u2013','\u2014','\u200B','\uFEFF','\u0020','\u00A0' };
+                //Представление текста в виде массива слов, приедение к единому регистру, сортировка по алфавиту
+                string[] words = text
+                    .ToLower()
+                    .Replace("\n", "")
+                    .Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                Array.Sort(words);
+
+                //Создание и заполнение словаря 
+                Dictionary<string, int> dic = new Dictionary<string, int>();
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (dic.ContainsKey(words[i]))
+                    {
+                        dic[words[i]]++;
+                    }
+                    else
+                    {
+                        dic.Add(words[i], 1);
+                    }
+                }
+                //запись результата в новый файл
+
+                string resultFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "frequencyDictionary.txt");
+                using (var writer = new StreamWriter(resultFile))
+
+                    foreach (var item in dic)
+                    {
+                        writer.WriteLine($"{item.Key}\t{item.Value}");
+                    }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+
+        }
+    }
+}
+
+  
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FrequencyDictionary;
+class Program
+{
+    public static void Main(string[] args)
+    {
+      
+        string sourceFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Voyna i mir.txt");
+        new App().Start(sourceFile);
+
+    }
+
+   
+
+
+
+
+}
